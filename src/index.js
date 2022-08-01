680fe85d0a13152e309400ca3aa58c90d2a2f5c2
= <michaelviloria.g@gmail.com>
2022-08-01 13:11:35 -0500

    Desafio finalizado

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import { Server as HttpServer } from "http";
+import { Server as IOServer } from "socket.io";
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+import routes from "./routes/routes.js";
+
+app.use(express.static("./public"));
+app.use("/api/productos", routes);
+
+// <------------------------- Configuracion de EJS ------------------------->
+
+app.set("view engine", ".ejs");
+app.set("views", "./src/views");
+
+// <------------------------- Clases ------------------------->
+import { optionsMySQL, optionsSQLite } from "./DB/options.js";
+
+import Contenedor from "./contenedor.js";
+const contenedor = new Contenedor({
+	options: optionsMySQL.options,
+	table: optionsMySQL.table,
+});
+
+import Chat from "./chat.js";
+const chat = new Chat({
+	options: optionsSQLite.options,
+	table: optionsSQLite.table,
+});
+
+// <------------------------- Sockets ------------------------->
+
+io.on("connection", async (socket) => {
+	console.log("Un cliente se ha conectado");
+	const responseProducts = await contenedor.getAll();
+	socket.emit("products", responseProducts);
+	const responseChats = await chat.getAll();
+	socket.emit("messages", responseChats);
+
+	socket.on("new-message", async (data) => {
+		await chat.save(data);
+		const response = await chat.getAll();
+		io.sockets.emit("messages", response);
+	});
+
+	socket.on("new-product", async (data) => {
+		await contenedor.save(data);
+		const response = await contenedor.getAll();
+		io.sockets.emit("products", response);
+	});
+});
+
+// <------------------------- Configuracion Servidor ------------------------->
+const PORT = process.env.PORT || 8080;
+
+const connectedServer = httpServer.listen(PORT, () => {
+	console.log(
+		`Servidor HTTP con Websockets escuchando en el puerto ${
+			connectedServer.address().port
+		}`
+	);
+});
+
+connectedServer.on("error", (error) =>
+	console.log(`Error en servidor ${error}`)
+);
